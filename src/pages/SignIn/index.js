c9adb404758a029cf7b5dfad44f68fe126884424
Cristiano Ferreira Mothe <cristianodevsystem@gmail.com>
2024-09-24 00:19:28 -0300

    feat: criar a tela de login

--- src/pages/SignIn/index.js
+++ src/pages/SignIn/index.js
@@ -1,10 +1,45 @@
-import React from 'react';
-import { View, Text } from 'react-native';
+import React from "react";
+import { Platform} from "react-native";
+import {
+  Background,
+  Container,
+  Logo,
+  AreaInput,
+  Input,
+  SubmitButton,
+  SubmitText,
+  Link,
+  LinkText,
+} from "./styles";
+import { useNavigation } from '@react-navigation/native'
 
-export default function SignIn(){
-  return(
-    <View>
-      <Text>TELA LOGIN</Text>
-    </View>
-  )
+export default function SignIn() {
+  const navigation = useNavigation();
+
+  return (
+    <Background>
+      <Container
+        behavior={Platform.OS === 'ios' ? 'padding' : '' }
+        enable
+      >
+        <Logo source={require("../../assets/Logo.png")} />
+
+        <AreaInput>
+          <Input placeholder="Seu email" />
+        </AreaInput>
+
+        <AreaInput>
+          <Input placeholder="Sua senha" />
+        </AreaInput>
+
+        <SubmitButton activeOpacity={0.8}>
+          <SubmitText>Acessar</SubmitText>
+        </SubmitButton>
+
+        <Link onPress={() => navigation.navigate('SignUp')}>
+          <LinkText>Criar uma conta</LinkText>
+        </Link>
+      </Container>
+    </Background>
+  );
 }
