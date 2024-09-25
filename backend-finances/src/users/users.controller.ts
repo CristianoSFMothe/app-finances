295931ca925c7e10d48ccdf7d833bb48d173f43f
Cristiano Ferreira Mothe <cristianodevsystem@gmail.com>
2024-09-24 21:41:28 -0300

    feat: funções para retornar todos os usuário, por ID e por email

--- backend-finances/src/users/users.controller.ts
+++ backend-finances/src/users/users.controller.ts
@@ -1,4 +1,4 @@
-import { Body, Controller, Post } from '@nestjs/common';
+import { Body, Controller, Get, Param, Post, Query } from '@nestjs/common';
 import { UsersService } from './users.service';
 import { CreateUserDto } from './dto/create-user.dto';
 
@@ -7,7 +7,22 @@ export class UsersController {
   constructor(private readonly usersService: UsersService) {}
 
   @Post()
-  create(@Body() data: CreateUserDto) {
+  async create(@Body() data: CreateUserDto) {
     return this.usersService.createUser(data);
+  }
+
+  @Get()
+  async findAllUser() {
+    return this.usersService.findAllUsers();
+  }
+
+  @Get(':id')
+  async getById(@Param('id') id: string) {
+    return this.usersService.getUserById(id);
+  }
+
+  @Get('email')
+  getByEmail(@Query('email') email: string) {
+    return this.usersService.getUserByEmail(email);
   }
 }
--- backend-finances/src/users/users.service.ts
+++ backend-finances/src/users/users.service.ts
@@ -1,4 +1,8 @@
-import { ConflictException, Injectable } from '@nestjs/common';
+import {
+  ConflictException,
+  Injectable,
+  NotFoundException,
+} from '@nestjs/common';
 import { PrismaService } from 'src/prisma/prisma.service';
 import { CreateUserDto } from './dto/create-user.dto';
 import * as bcrypt from 'bcrypt';
@@ -32,5 +36,56 @@ export class UsersService {
         balance: true,
       },
     });
+  }
+
+  async findAllUsers() {
+    return this.prisma.user.findMany({
+      select: {
+        id: true,
+        name: true,
+        email: true,
+        balance: true,
+      },
+    });
+  }
+
+  async getUserById(id: string) {
+    const user = await this.prisma.user.findUnique({
+      where: { id },
+      select: {
+        id: true,
+        name: true,
+        email: true,
+        balance: true,
+        created_at: true,
+        updated_at: true,
+      },
+    });
+
+    if (!user) {
+      throw new NotFoundException('Usuário não encontrado');
+    }
+
+    return user;
+  }
+
+  async getUserByEmail(email: string) {
+    const user = await this.prisma.user.findUnique({
+      where: { email },
+      select: {
+        id: true,
+        name: true,
+        email: true,
+        balance: true,
+        created_at: true,
+        updated_at: true,
+      },
+    });
+
+    if (!user) {
+      throw new NotFoundException('Usuário não encontrado');
+    }
+
+    return user;
   }
 }
