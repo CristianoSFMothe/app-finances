29d98c883879899ff95136f27c88bc7a0253f4f0
Cristiano Ferreira Mothe <cristianodevsystem@gmail.com>
2024-09-24 22:52:06 -0300

    feat: autenticação

--- backend-finances/src/app.module.ts
+++ backend-finances/src/app.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { PrismaModule } from './prisma/prisma.module';
 import { UsersModule } from './users/users.module';
 import { ReceivesModule } from './receives/receives.module';
+import { AuthModule } from './auth/auth.module';
 
 @Module({
-  imports: [PrismaModule, UsersModule, ReceivesModule],
+  imports: [PrismaModule, UsersModule, ReceivesModule, AuthModule],
   controllers: [],
   providers: [],
 })
