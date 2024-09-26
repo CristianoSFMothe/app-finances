5ee2754968a64568de89389a10bf6c10400a2aab
Cristiano Ferreira Mothe <cristianodevsystem@gmail.com>
2024-09-26 20:48:35 -0300

    feat: utilizando o loading de cadastro

--- mobile/src/contexts/auth.js
+++ mobile/src/contexts/auth.js
@@ -4,13 +4,14 @@ import { useNavigation } from '@react-navigation/native'
 export const AuthContext = createContext({}) 
 
 const AuthProvider = ({ children })  => {
-  const [user, setUser] = useState({
-    name: 'Cristiano Teste'
-  })
+  const [user, setUser] = useState(null)
+  const [loadingAuth, setLoadingAuth] = useState(false)
 
   const navigation = useNavigation();
 
   const signUp = async (name, email, password) => {
+    setLoadingAuth(true);
+
     try{
       const response = await api.post('/users', {
         name,
@@ -18,15 +19,19 @@ const AuthProvider = ({ children })  => {
         password
       })
 
+      setLoadingAuth(false);
+
       navigation.goBack()
 
     } catch(err) {
-      console.log('ERRO AO CADASTRAR', err)
+      console.log('ERRO AO CADASTRAR', err);
+
+      setLoadingAuth(false)
     }
   }
 
   return(
-    <AuthContext.Provider value={{ user, signUp }}>
+    <AuthContext.Provider value={{ user, signUp, loadingAuth }}>
       {children}
     </AuthContext.Provider>
   )
--- mobile/src/pages/SignUp/index.js
+++ mobile/src/pages/SignUp/index.js
@@ -1,5 +1,5 @@
-import React, { useContext, useState } from 'react';
-import { Platform } from 'react-native';
+import React, { useContext, useState } from "react";
+import { Platform, ActivityIndicator } from "react-native";
 import {
   Background,
   Container,
@@ -8,55 +8,57 @@ import {
   SubmitButton,
   SubmitText,
 } from "../SignIn/styles";
-import { AuthContext } from '../../contexts/auth';
+import { AuthContext } from "../../contexts/auth";
 
-export default function SignUp(){
-  const { signUp } = useContext(AuthContext);
-  const [name, setName] = useState('')
-  const [email, setEmail] = useState('')
-  const [password, setPassword] = useState('')
+export default function SignUp() {
+  const { signUp, loadingAuth } = useContext(AuthContext);
+
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
 
   const handleSignUp = () => {
+    if (name === '' || email === '' || password === '') return;
+    
     signUp(name, email, password);
-  }
+  };
 
-  return(
+  return (
     <Background>
-      <Container
-        behavior={Platform.OS === 'ios' ? 'padding' : '' }
-        enable      
-      >
+      <Container behavior={Platform.OS === "ios" ? "padding" : ""} enable>
+        <AreaInput>
+          <Input
+            placeholder="Nome"
+            value={name}
+            onChangeText={(text) => setName(text)}
+          />
+        </AreaInput>
 
-      <AreaInput>
-        <Input 
-          placeholder="Nome"
-          value={name}
-          onChangeText={(text) => setName(text)}
-        />
-      </AreaInput>
+        <AreaInput>
+          <Input
+            placeholder="Seu email"
+            value={email}
+            onChangeText={(text) => setEmail(text)}
+          />
+        </AreaInput>
 
-      <AreaInput>
-        <Input 
-          placeholder="Seu email"
-          value={email}
-          onChangeText={(text) => setEmail(text)}
-        />
-      </AreaInput>
+        <AreaInput>
+          <Input
+            placeholder="Sua senha"
+            value={password}
+            onChangeText={(text) => setPassword(text)}
+            secureTextEntry={true}
+          />
+        </AreaInput>
 
-      <AreaInput>
-        <Input 
-          placeholder="Sua senha"
-          value={password}
-          onChangeText={(text) => setPassword(text)}
-          secureTextEntry={true}
-        />
-      </AreaInput>
-
-      <SubmitButton activeOpacity={0.8} onPress={handleSignUp}>
-        <SubmitText>Cadastrar</SubmitText>
-      </SubmitButton>
-
+        <SubmitButton activeOpacity={0.8} onPress={handleSignUp}>
+          {loadingAuth ? (
+            <ActivityIndicator size="18" color="#fff" />
+          ) : (
+            <SubmitText>Cadastrar</SubmitText>
+          )}
+        </SubmitButton>
       </Container>
     </Background>
-  )
+  );
 }
