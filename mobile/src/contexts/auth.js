f11a1229d907c1f5037528338bdbdaf44d5b1677
Cristiano Ferreira Mothe <cristianodevsystem@gmail.com>
2024-09-28 14:57:25 -0300

    feat: criação do login

--- mobile/src/contexts/auth.js
+++ mobile/src/contexts/auth.js
@@ -1,40 +1,77 @@
-import React, { createContext, useState } from 'react'
-import api from '../services/api'
-import { useNavigation } from '@react-navigation/native'
-export const AuthContext = createContext({}) 
+import React, { createContext, useState } from 'react';
 
-const AuthProvider = ({ children })  => {
-  const [user, setUser] = useState(null)
-  const [loadingAuth, setLoadingAuth] = useState(false)
+import api from '../services/api';
+import { useNavigation } from '@react-navigation/native';
+
+export const AuthContext = createContext({});
+
+function AuthProvider({ children }){
+  const [user, setUser] = useState(null); 
+  const [loadingAuth, setLoadingAuth] = useState(false);
 
   const navigation = useNavigation();
 
-  const signUp = async (name, email, password) => {
+
+  const signUp = async (email, password, nome) => {
     setLoadingAuth(true);
 
     try{
       const response = await api.post('/users', {
+       name: nome,
+       password: password,
+       email: email,
+      })
+      setLoadingAuth(false);
+
+      navigation.goBack();
+
+
+    }catch(err){
+      console.log("ERRO AO CADASTRAR", err);
+      setLoadingAuth(false);
+    }
+  }
+
+  const signIn = async (email, password) => {
+    setLoadingAuth(true);
+
+    try{
+      const response = await api.post('/login', {
+        email: email,
+        password: password
+      })
+
+      const { id, name, token } = response.data;
+
+      const data = {
+        id,
+        name,
+        token,
+        email,
+      };
+
+      api.defaults.headers['Authorization'] = `Bearer ${token}`;
+
+      setUser({
+        id,
         name,
         email,
-        password
       })
 
       setLoadingAuth(false);
 
-      navigation.goBack()
-
-    } catch(err) {
-      console.log('ERRO AO CADASTRAR', err);
-
-      setLoadingAuth(false)
+    }catch(err){
+      console.log("ERRO AO LOGAR ", err);
+      setLoadingAuth(false);
     }
+
   }
 
   return(
-    <AuthContext.Provider value={{ signed: !!user, user, signUp, loadingAuth }}>
+    <AuthContext.Provider value={{ signed: !!user, user, signUp, signIn, loadingAuth }}>
       {children}
     </AuthContext.Provider>
   )
 }
 
-export default AuthProvider
+export default AuthProvider;
--- mobile/src/pages/SignIn/index.js
+++ mobile/src/pages/SignIn/index.js
@@ -1,45 +1,77 @@
-import React from "react";
-import { Platform} from "react-native";
-import {
-  Background,
-  Container,
-  Logo,
-  AreaInput,
-  Input,
-  SubmitButton,
+import React, { useState, useContext } from 'react';
+import { Platform, ActivityIndicator } from 'react-native';
+
+import { 
+  Background, 
+  Container, 
+  Logo, 
+  AreaInput, 
+  Input, 
+  SubmitButton, 
   SubmitText,
   Link,
-  LinkText,
-} from "./styles";
-import { useNavigation } from '@react-navigation/native'
+  LinkText
+} from './styles';
 
-export default function SignIn() {
+import { useNavigation } from '@react-navigation/native';
+
+import { AuthContext } from '../../contexts/auth'
+
+export default function SignIn(){
   const navigation = useNavigation();
+  const { signIn, loadingAuth } = useContext(AuthContext);
 
-  return (
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+
+  const handleLogin = () => {
+    signIn(email, password);
+  }
+
+  return(
     <Background>
+
       <Container
-        behavior={Platform.OS === 'ios' ? 'padding' : '' }
-        enable
+        behavior={Platform.OS === 'ios' ? 'padding' : ''}
+        enabled
       >
-        <Logo source={require("../../assets/Logo.png")} />
+        <Logo
+          source={require('../../assets/Logo.png')}
+        />
 
         <AreaInput>
-          <Input placeholder="Seu email" />
+          <Input
+            placeholder="Seu email"
+            value={email}
+            onChangeText={ (text) => setEmail(text) }
+          />
         </AreaInput>
 
         <AreaInput>
-          <Input placeholder="Sua senha" />
+          <Input
+            placeholder="Sua senha"
+            value={password}
+            onChangeText={ (text) => setPassword(text) }
+          />
         </AreaInput>
 
-        <SubmitButton activeOpacity={0.8}>
-          <SubmitText>Acessar</SubmitText>
+        <SubmitButton activeOpacity={0.8} onPress={handleLogin}>
+          {
+            loadingAuth ? (
+              <ActivityIndicator size={20} color="#FFF" />
+            ) : (
+              <SubmitText>Acessar</SubmitText>
+            )
+          }
         </SubmitButton>
 
-        <Link onPress={() => navigation.navigate('SignUp')}>
-          <LinkText>Criar uma conta</LinkText>
+        <Link onPress={ () => navigation.navigate('SignUp') }>
+          <LinkText>Criar uma conta!</LinkText>
         </Link>
+
       </Container>
+
     </Background>
-  );
+  )
 }
