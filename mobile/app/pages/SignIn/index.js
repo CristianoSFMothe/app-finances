fbc15b0606a5e7f899175f637a644c83ed723bed
Cristiano Ferreira Mothe <cristianodevsystem@gmail.com>
2024-10-03 23:43:34 -0300

    feat: recriando a tela de login

--- mobile/app/pages/SignIn/index.js
+++ mobile/app/pages/SignIn/index.js
@@ -1,10 +1,60 @@
-import React from 'react';
-import { View, Text } from 'react-native';
+import React, { useState } from "react";
+import { Platform } from "react-native";
 
-export default function SignIn(){
-  return(
-    <View>
-      <Text>TELA LOGIN</Text>
-    </View>
-  )
+import {
+  Background,
+  Container,
+  Logo,
+  AreaInput,
+  Input,
+  Label,
+  IconContainer,
+  SubmitButton,
+  SubmitText,
+  Link,
+  LinkText,
+} from "./styles";
+
+import Ionicons from "@expo/vector-icons/Ionicons";
+import { useNavigation } from "@react-navigation/native";
+
+export default function SignIn() {
+  const [hidePassword, setHidePassword] = useState(true);
+  const navigation = useNavigation();
+
+  return (
+    <Background>
+      <Container
+        behavior={Platform.OS === 'ios' ? 'padding' : ''}
+        enabled
+      >
+        <Logo source={require("../../assets/Logo.png")} />
+
+        <Label>Email</Label>
+        <AreaInput>
+          <Input placeholder="Seu E-mail" keyboardType="email-address" />
+        </AreaInput>
+
+        <Label>Senha</Label>
+        <AreaInput>
+          <Input placeholder="Sua Senha" secureTextEntry={hidePassword} />
+          <IconContainer onPress={() => setHidePassword(!hidePassword)}>
+            <Ionicons
+              name={hidePassword ? "eye-off" : "eye"}
+              size={25}
+              color="#737373"
+            />
+          </IconContainer>
+        </AreaInput>
+
+        <SubmitButton activeOpacity={0.9}>
+          <SubmitText>Entrar</SubmitText>
+        </SubmitButton>
+
+        <Link onPress={() => navigation.navigate('SignUp')}>
+          <LinkText>Criar uma conta</LinkText>
+        </Link>
+      </Container>
+    </Background>
+  );
 }
---
+++ mobile/app/theme.js
@@ -0,0 +1,9 @@
+export const theme = {
+  background: '#f0f4ff',
+  white: '#fff',
+  darkText: '#121212',
+  buttonBackground: '#3b3bdf',
+  linkText: '#171717',
+  submitText: '#FFF',
+  border: '#00b67a'
+};
