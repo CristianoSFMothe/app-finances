7ce3e39fbb1c3fc2ac51817566da609066fe4b7e
Cristiano Ferreira Mothe <cristianodevsystem@gmail.com>
2024-10-03 21:52:28 -0300

    feat: recriando o projeto

--- mobile/src/pages/Home/styles.js
+++
@@ -1,7 +0,0 @@
-import styled from "styled-components/native";
-import { theme } from "../../theme"; 
-
-export const Background = styled.SafeAreaView`
-  flex: 1;
-  background-color: ${theme.background};
-`;
--- mobile/src/services/api.js
+++
@@ -1,7 +0,0 @@
-import axios from "axios";
-
-const api = axios.create({
-  baseURL: 'http://10.0.0.40:8080'
-})
-
-export default api
